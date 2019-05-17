130cfb7ed4ff51c682c99f08d5ec6abf32fcf3e2
CaiLiangl <291961214@qq.com>
2019-05-17 21:48:44 +0800

    21.48

--- src/com/atguigu/com/test/HelloEgit.java
+++ src/com/atguigu/com/test/HelloEgit.java
@@ -4,7 +4,7 @@ public class HelloEgit {
 
 	public static void main(String[] args) {
 		System.out.println("aaaa");
-		System.out.println("bbbb");
+		System.out.println("dddd");
 		System.out.println("cccc");
 	}
 
